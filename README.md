934611993efae39786d638b6090a853a98d6d110
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2019-10-21 11:06:02 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### JavaFX + OpenGL
 
 https://www.youtube.com/watch?v=-uAFkm5kc7M
+
+# Conclusion
+
+Conclusion is to use OpenGL with Swing and embed swing panels to javafx ones
+
+https://docs.oracle.com/javafx/8/embed_swing/jfxpub-embed_swing.htm
---
+++ src/main/java/javafxAndOpengl/Test1.java
@@ -0,0 +1,35 @@
+package javafxAndOpengl;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+public class Test1 extends Application {
+
+    @Override
+    public void start (Stage stage) {
+        final SwingNode swingNode = new SwingNode();
+
+        createSwingContent(swingNode);
+
+        StackPane pane = new StackPane();
+        pane.getChildren().add(swingNode);
+
+        stage.setTitle("Swing in JavaFX");
+        stage.setScene(new Scene(pane, 250, 150));
+        stage.show();
+        }
+
+    private void createSwingContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                swingNode.setContent(new JButton("Click me!"));
+            }
+        });
+    }
+}
